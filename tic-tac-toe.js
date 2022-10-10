b97e4ac236c5ee755b4539852c61ada0d7e5757d
Jamilla Morgan <jamillamorgan02@gmail.com>
2022-10-10 04:30:05 -0500

    Completed Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let boardSquares = document.querySelectorAll("div").className = "board";
+
+    boardSquares.classList.add("square");
+});
