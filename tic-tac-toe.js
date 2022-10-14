e74c951c1a5cda3148f351ffc104542c553a2e9d
Jamilla Morgan <jamillamorgan02@gmail.com>
2022-10-13 20:42:56 -0500

    Completed Most of Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let gameState = [];
+    const x_Class = "x";
+    const o_Class = "o";
+    let xTurn = true;
+
     let boardSquares = document.getElementById("board").querySelectorAll('div');
 
     var count;
@@ -7,5 +12,30 @@ document.addEventListener("DOMContentLoaded", function () {
         boardSquares[count].setAttribute("class", "square");
     }
 
-    let state = [];
+    boardSquares.forEach(cell => {
+        cell.addEventListener('click', clickFunction, { once: true })
+    })
+
+    function clickFunction(e) {
+        console.log("Clicked thing")
+        const cell = e.target;
+        let currentClass = xTurn ? x_Class : o_Class
+        
+        placeMark(cell, currentClass)
+
+        xTurn = !xTurn;
+    }
+
+    function placeMark(cell, currentClass) {
+        if(currentClass = x_Class) {
+            cell.classList.add("square.X");
+            cell.innerHTML = "X";
+        }
+
+        else if(currentClass = o_Class) {
+            cell.innerHTML = "O";
+            cell.classList.add("square.O");
+        }
+    }
+
 });
