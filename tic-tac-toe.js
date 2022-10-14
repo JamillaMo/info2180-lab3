c61503fe17719585460902a75c20c399eea3c830
Jamilla Morgan <jamillamorgan02@gmail.com>
2022-10-14 17:18:40 -0500

    Completed Most of Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let gameState = [];
+    let count;
+    let xGameState = [];
+    let oGameState = [];
+    let winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    let boardSquares = document.getElementById("board").querySelectorAll('div');
+    let xTurn = true;
     const x_Class = "x";
     const o_Class = "o";
-    let xTurn = true;
-
-    let boardSquares = document.getElementById("board").querySelectorAll('div');
-
-    var count;
 
     for (count = 0; count < boardSquares.length; count++) {
         boardSquares[count].setAttribute("class", "square");
@@ -17,15 +17,31 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
     function clickFunction(e) {
-        console.log("Clicked thing")
         const cell = e.target;
-        let currentClass = xTurn == true ? x_Class : o_Class
+        let currentClass = xTurn ? x_Class : o_Class
 
         placeMark(cell, currentClass)
 
-        gameState.push(cell)
-        console.log(cell)
-        
+        function getElementIndex(e) {
+            var index = 0;
+            while ((e = e.previousElementSibling)) {
+                index++;
+            }
+            return index;
+        }
+
+        getElementIndex(cell);
+
+        if (xTurn == true) {
+            xGameState.push(getElementIndex(cell));
+        }
+
+        else {
+            oGameState.push(getElementIndex(cell));
+        }
+
+        checkForWin();
+
         xTurn = !xTurn;
 
     }
@@ -47,9 +63,66 @@ document.addEventListener("DOMContentLoaded", function () {
             e.target.classList.add('hover');
         });
 
-        cell.addEventListener('mouseout', function(e) {
+        cell.addEventListener('mouseout', function (e) {
             e.target.classList.remove('hover');
-          });
+        });
     });
+
+    function checkForWin() {
+        //If the elements in xGameState are in one of the win combinations
+        for (count = 0; count < winCombinations.length; count++) {
+            let win;
+            //console.log("winner");
+            //console.log(winCombinations[count].every(checkIfIn));
+            win = winCombinations[count].every(checkIfIn);
+            //console.log(winCombinations[count]);
+            console.log(xTurn);
+
+            if (win == true) /*and the textContent == "XXX" or "OOO" */ {
+                boardSquares.forEach(function (cell, index) {
+                    cell.removeEventListener('click', clickFunction, { once: true })
+
+                    cell.addEventListener('mouseover', function (e) {
+                        e.target.classList.replace('hover', 'active');
+                    });
+                });
+
+                document.getElementById("status").setAttribute("class", "you-won");
+                console.log("winner");
+                console.log(boardSquares.forEach(function(cell) {
+                    cell.textContent;
+                }));
+                
+                //If all spaces are filled and there are no winners, there is a draw.
+
+                if(xTurn == true) {
+                    document.getElementById("status").textContent = "Congratulations! X is the winner!";
+                }
+                
+                else if(xTurn == false) {
+                    document.getElementById("status").textContent = "Congratulations! O is the winner!";
+                }
+            }
+
+            // else if() {
+            //     //
+            // }
+
+        }
+
+        //Account for a draw
+    }
+
+    function checkIfIn(x) {
+        if (xTurn == true) {
+            //console.log(xGameState);
+            return x in xGameState;
+        }
+
+        else if (xTurn == false) {
+            //console.log(x);
+            return x in oGameState;
+        }
+    }
 
 });
