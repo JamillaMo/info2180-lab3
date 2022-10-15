f2358fc77c7fb4ba501baecd3c9c8c08e2a9a3c3
Jamilla Morgan <jamillamorgan02@gmail.com>
2022-10-15 01:19:21 -0500

    Completed Exercises 1 through to 6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     let count;
     let win;
-    let xGameState = [];
-    let oGameState = [];
+    let xGameState = [, , , , , , , , ];
+    let oGameState = [, , , , , , , , ];
     let winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     let boardSquares = document.getElementById("board").querySelectorAll('div');
     let xTurn = true;
@@ -14,10 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     boardSquares.forEach(cell => {
-        cell.addEventListener('click', clickFunction, { once: true })
+        cell.addEventListener('click', clickFunction, {})
     })
 
-    
     function getElementIndex(e) {
         var index = 0;
         while ((e = e.previousElementSibling)) {
@@ -30,21 +29,25 @@ document.addEventListener("DOMContentLoaded", function () {
         const cell = e.target;
         let currentClass = xTurn ? x_Class : o_Class
 
-        placeMark(cell, currentClass)
+        if (!win) {
+            if (xTurn == true && boardSquares[getElementIndex(cell)].innerHTML == "") {
+                xGameState[getElementIndex(cell)] = true;
+                // xGameState.push(getElementIndex(cell));
+                placeMark(cell, currentClass);
+                xTurn = !xTurn;
+            }
 
-        // getElementIndex(cell);
-
-        if (xTurn == true) {
-            xGameState.push(getElementIndex(cell));
+            else if(xTurn == false && boardSquares[getElementIndex(cell)].innerHTML == ""){
+                oGameState[getElementIndex(cell)] = true;
+                // oGameState.push(getElementIndex(cell));
+                placeMark(cell, currentClass);
+                xTurn = !xTurn;
+            }
+            checkForWin();
         }
+        // console.log(getElementIndex(e.target) + " index");
 
-        else {
-            oGameState.push(getElementIndex(cell));
-        }
-
-        checkForWin();
-
-        xTurn = !xTurn;
+        // xTurn = !xTurn;
 
     }
 
@@ -54,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
             cell.innerHTML = "X";
         }
 
-        else if (currentClass == o_Class) {
+        else {
             cell.innerHTML = "O";
             cell.classList.add("O");
         }
@@ -71,66 +74,132 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function checkForWin() {
+        let winAmount = [0,0];
         //If the elements in xGameState are in one of the win combinations
-        for (count = 0; count < winCombinations.length; count++) {
+        //for (count = 0; count < winCombinations.length - 1; count++) {
             //console.log("winner");
             //console.log(winCombinations[count].every(checkIfIn));
-            win = winCombinations[count].every(checkIfIn);
+            //win = winCombinations[count].every(checkIfIn);
             //console.log(winCombinations[count]);
-            console.log(xTurn);
+            //console.log(xTurn);
 
-            if (win == true) /*and the textContent == "XXX" or "OOO" */ {
-                // for(i = 0; i < winCombinations[count].length; i++) {
-                //     console.log(i);
-                //     console.log(winCombinations[count] + " " + "Hi");
-                //     console.log(winCombinations[count][i] + " " + "Hi 3");
-                //     console.log(winCombinations[count][i].textContent + " " + "Hi 2");
-                // }
-                boardSquares.forEach(function (cell, index) {
-                    cell.removeEventListener('click', clickFunction, { once: true })
+            // if (win == true) /*and the textContent == "XXX" or "OOO" */ {
+            //     // for(i = 0; i < winCombinations[count].length; i++) {
+            //     //     console.log(i);
+            //     //     console.log(winCombinations[count] + " " + "Hi");
+            //     //     console.log(winCombinations[count][i] + " " + "Hi 3");
+            //     //     console.log(winCombinations[count][i].textContent + " " + "Hi 2");
+            //     // }
+            //     boardSquares.forEach(function (cell, index) {
+            //         cell.removeEventListener('click', clickFunction, { once: true })
 
-                    cell.addEventListener('mouseover', function (e) {
-                        e.target.classList.replace('hover', 'active');
-                    });
-                });
+            //         cell.addEventListener('mouseover', function (e) {
+            //             e.target.classList.replace('hover', 'active');
+            //         });
+            //     });
 
-                document.getElementById("status").setAttribute("class", "you-won");
-                console.log("winner");
-                console.log(boardSquares.forEach(function (cell) {
-                    cell.textContent;
-                }));
+            //     document.getElementById("status").setAttribute("class", "you-won");
+            //     console.log("winner");
+            //     console.log(boardSquares.forEach(function (cell) {
+            //         cell.textContent;
+            //     }));
 
-                //If all spaces are filled and there are no winners, there is a draw.
+            //     //If all spaces are filled and there are no winners, there is a draw.
 
-                if (xTurn == true) {
-                    document.getElementById("status").textContent = "Congratulations! X is the winner!";
-                }
 
-                else if (xTurn == false) {
-                    document.getElementById("status").textContent = "Congratulations! O is the winner!";
+
+            //     if (xTurn == true) {
+            //         document.getElementById("status").textContent = "Congratulations! X is the winner!";
+            //     }
+
+            //     else if (xTurn == false) {
+            //         document.getElementById("status").textContent = "Congratulations! O is the winner!";
+            //     }
+            // }
+
+            for(count = 0; count <= winCombinations.length-1; count++){
+                winAmount[0] = 0;
+                winAmount[1] = 0;
+                for(let inner = 0; inner <= winCombinations[count].length-1; inner++){
+                    if(xGameState[winCombinations[count][inner]] == true){
+                        winAmount[0]++;
+                        if(winAmount[0] == 3){
+                            win = true;
+                            document.getElementById("status").textContent = "Congratulations! X is the winner!";
+                            document.getElementById("status").setAttribute("class", "you-won");
+                        }
+                    }
+                    if(oGameState[winCombinations[count][inner]] == true){
+                        winAmount[1]++;
+                        if(winAmount[1] == 3){
+                            win = true;
+                            document.getElementById("status").textContent = "Congratulations! O is the winner!";
+                            document.getElementById("status").setAttribute("class", "you-won");
+                        }
+                    }
                 }
             }
 
-            // else if() {
-            //     //
-            // }
+        // for (count = 0; count < winCombinations.length; count++) {
+        //     winAmount[0] = 0;
+        //     winAmount[1] = 0;
+
+        //     for (innercount = 0; innercount < winCombinations[count].length; innercount++) {
+        //         if (winCombinations[count][innercount] in xGameState) {
+        //             console.log(count, innercount);
+        //             winAmount[0]++;
+        //             //console.log(/*winCombinations[count][innercount], xGameState, */winAmount[0] + " x");
+
+        //             if (winAmount[0] == 3) {
+        //                     win = true;
+        //                     console.log("winner");
+        //                     document.getElementById("status").setAttribute("class", "you-won");
+        //                     document.getElementById("status").textContent = "Congratulations! X is the winner!";
+        //                 }
+        //         }
+                // if (winAmount[0] == 3) {
+                //     win = true;
+                //     console.log("winner");
+                //     document.getElementById("status").setAttribute("class", "you-won");
+                //     document.getElementById("status").textContent = "Congratulations! X is the winner!";
+                // }
+
+                // if (winCombinations[count][innercount] in oGameState) {
+                //     winAmount[1]++;
+                //     //console.log(winAmount[1]);
+
+                //     if (winAmount[1] == 3) {
+                //         win = true;
+                //         console.log("winner");
+                //         document.getElementById("status").setAttribute("class", "you-won");
+                //         document.getElementById("status").textContent = "Congratulations! O is the winner!";
+                //     }
+                // }
+                // if (winAmount[1] == 3) {
+                //     win = true;
+                //     console.log("winner");
+                //     document.getElementById("status").setAttribute("class", "you-won");
+                //     document.getElementById("status").textContent = "Congratulations! O is the winner!";
+                // }
+            //}
+        //}
 
         }
 
         //Account for a draw
-    }
+    //}
 
-    function checkIfIn(x) {
-        if (xTurn == true) {
-            //console.log(xGameState);
-            return x in xGameState;
-        }
+    // function checkIfIn(x) {
+    //     if (xTurn == true) {
+    //         //console.log(xGameState);
+    //         return x in xGameState;
+    //     }
 
-        else if (xTurn == false) {
-            //console.log(x);
-            return x in oGameState;
-        }
-    }
+    //     else if (xTurn == false) {
+    //         //console.log(x);
+    //         return x in oGameState;
+    //     }
+    // }
 
     resetBtn = document.getElementsByClassName("btn")[0]
     resetBtn.addEventListener("click", resetGame);
@@ -138,12 +207,15 @@ document.addEventListener("DOMContentLoaded", function () {
     function resetGame() {
         let gameStatus;
 
-        console.log("Clicked");
+        win = false;
+
         gameStatus = document.getElementById("status");
         gameStatus.textContent = "Move your mouse over a square and click to play an X or an O.";
         gameStatus.removeAttribute("class", "you-won");
 
         function removeMark(cell) {
+            cell.classList.remove("X");
+            cell.classList.remove("O");
             cell.innerHTML = "";
         }
 
@@ -151,21 +223,26 @@ document.addEventListener("DOMContentLoaded", function () {
             removeMark(boardSquares[count]);
         }
 
-        boardSquares.forEach(function (cell, index) {
-            cell.addEventListener('mouseover', function (e) {
-                e.target.classList.add('hover');
-            });
+        // boardSquares.forEach(function (cell, index) {
+        //     cell.addEventListener('mouseover', function (e) {
+        //         e.target.classList.add('hover');
+        //     });
+
+        //     cell.addEventListener('mouseout', function (e) {
+        //         e.target.classList.remove('hover');
+        //     });
+        // });
+
+        // boardSquares.forEach(cell => {
+        //     cell.addEventListener('click', clickFunction, { once: true })
+        // })
+
+        xGameState = [, , , , , , , , ];
+        oGameState = [, , , , , , , , ];
     
-            cell.addEventListener('mouseout', function (e) {
-                e.target.classList.remove('hover');
-            });
-        });
+        //xTurn = true;
 
-        boardSquares.forEach(cell => {
-            cell.addEventListener('click', clickFunction, { once: true })
-        })
-
-
+        //Collaborators: David Lynch
     }
 
 });
